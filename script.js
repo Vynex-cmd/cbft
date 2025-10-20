89f159529ac985a6e269f8743c8681be287fac54
Vynex <83349493+Vynex-cmd@users.noreply.github.com>
2025-10-20 13:47:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
-  const name = "CBFT";
+  const name = "Slyce";
   let nameText = '';
   let nameIndex = 0;
   let isNameDeleting = false;
@@ -670,3 +670,4 @@ document.addEventListener('DOMContentLoaded', () => {
   typeWriterStart();
 
 });
+
